d1077f75e2fdd1f1e0ea4d012bd61524aafa2edb
Alok <alokamit161@gmail.com>
2024-06-07 09:28:44 +0530

    Initial Commit

---
+++ api/routes/ToDoRoute.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  getToDo,
+  saveToDo,
+  deleteToDo,
+  updateToDo,
+} = require("../controllers/ToDoController");
+
+router.get("/", getToDo);
+
+router.post("/save", saveToDo);
+
+router.post("/update", updateToDo);
+
+router.post("/delete", deleteToDo);
+
+module.exports = router;
